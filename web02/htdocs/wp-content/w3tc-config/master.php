af33ad24b7e61a42204339e6ae40282ab55ca7c9
Lorenz Hänggi <lolo8304@gmail.com>
2015-12-11 22:27:31 +0100

    update plugins

--- web02/htdocs/wp-content/w3tc-config/master.php
+++ web02/htdocs/wp-content/w3tc-config/master.php
@@ -420,7 +420,7 @@ return array(
 	'browsercache.other.etag' => true,
 	'browsercache.other.w3tc' => true,
 	'browsercache.other.replace' => false,
-	'browsercache.timestamp' => '1446983722',
+	'browsercache.timestamp' => '1447518449',
 	'browsercache.replace.exceptions' => array(
 		0 => '',
 	),
